e73fd8ad5c9ab8c0ba9aa9ce12071c96c0b3d161
ailson11 <sousa.ailson@escola.pr.gov.br>
2025-06-11 11:30:42 -0300

    06

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,7 @@
 criaCartao(
     'Neymar',
     'Quem é neymar?',
-    'Neymar é um dos maiores jogadores de futebol do Brasil, com passagens por Santos, Barcelona e PSG. Destaca-se pela habilidade e velocidade, sendo também uma estrela da seleção brasileira.
-
-
-
-
-'
+    'Neymar é um dos maiores jogadores de futebol do Brasil, com passagens por Santos, Barcelona e PSG. Destaca-se pela habilidade e velocidade, sendo também uma estrela da seleção brasileira.'
 )
 
 criaCartao(
@@ -19,10 +14,4 @@ criaCartao(
     'Clubes',
     'Por quantos clubes Neymar já atuou?',
     'Neymar já atuou por um total de 3 clubes: Santos FC, Barcelona, PSG e atualmente ocorreu sua volta para o clube Santos'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
 )
